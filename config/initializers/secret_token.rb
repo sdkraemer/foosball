9440db92359e3b4bc5bb7fdf4f474a98aa8368e8
Scott Kraemer <tfcmaster9@gmail.com>
2014-05-03 17:09:56 -0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoosballApp::Application.config.secret_key_base = '09f22af4c0c2d92106c9bcef6cc633b018c116cbe05887c32867fc2bde92f9231baab1839ab24c4e17a4f0432e9b0b5ed613605bdbb37aa33648ebf6719cd384'
