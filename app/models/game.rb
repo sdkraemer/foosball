57ec5d68f6d01d915648be57d3c3e358dd8f59b4
Scott Kraemer <tfcmaster9@gmail.com>
2014-05-27 22:42:57 -0700

    removed accepts_nested_attributes_for

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,4 @@
 class Game < ActiveRecord::Base
 	belongs_to :teamone, class_name: 'Team', foreign_key: 'teamoneid'
 	belongs_to :teamtwo, class_name: 'Team', foreign_key: 'teamtwoid'
-	accepts_nested_attributes_for :teams
 end
