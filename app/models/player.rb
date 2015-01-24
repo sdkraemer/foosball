b4d72a803a8c9128ee7c020af3ada8a55827ab03
Scott Kraemer <tfcmaster9@gmail.com>
2015-01-24 11:30:55 -0700

    Added goals per stick in Player model. Refactored statistics methods in Player model as well.

--- app/models/player.rb
+++ app/models/player.rb
@@ -21,15 +21,16 @@ class Player < ActiveRecord::Base
 
 	#subtracts own goals
 	def total_goals
-		goals = self.goals
-		scored_count = goals.scored_goal.count
-		own_goals_count = goals.own_goal.count
+		scored_goals = Game.completed.joins(:goals).merge(Goal.scored_goal).select("goals.id").where(:goals => {:player_id => self.id}).count
+		own_goals = Game.completed.joins(:goals).merge(Goal.own_goal).select("goals.id").where(:goals => {:player_id => self.id}).count
 		
-		@total_goals ||= (scored_count - own_goals_count)
+		@total_goals ||= (scored_goals - own_goals)
 	end
 
 	def player_games
-		@player_games ||= Game.completed.joins(:teams).select("games.*, teams.winner").where(:teams => {:id => Position.select(:team_id).uniq.where(:player_id => self.id)} )
+		@player_games ||= Game.uniq.completed.joins(:teams).joins(:positions).where(:positions => {:player_id => self.id})
+
+		#@player_games ||= Game.completed.joins(:teams).select("games.id").where(:teams => {:id => Position.select(:team_id).uniq.where(:player_id => self.id)} )
 	end
 
 	def goals_per_game
@@ -41,12 +42,21 @@ class Player < ActiveRecord::Base
 		end
 	end
 
+	def goals_per_stick
+		total_positions = Game.completed.joins(:positions).where(:positions => {player_id: self.id}).count
+		if total_positions > 0
+			(total_goals*1.0)/total_positions
+		else
+			0
+		end
+	end
+
 	def player_wins
-		@player_wins ||= player_games.where("teams.winner" => true).count("games.id")
+		@player_wins ||= player_games.merge(Team.winner).count
 	end
 
 	def player_losses
-		@player_losses ||= player_games.where("teams.winner" => false).count("games.id")
+		@player_losses ||= player_games.merge(Team.loser).count
 	end
 
 	def win_pct
