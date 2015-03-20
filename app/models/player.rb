deae949f6668548f1ac68faf95491b34293ba990
Scott Kraemer <tfcmaster9@gmail.com>
2015-03-20 11:44:38 -0700

    Refactored player queries

--- app/models/player.rb
+++ app/models/player.rb
@@ -40,7 +40,7 @@ class Player < ActiveRecord::Base
 	end
 
 	def goals_per_stick
-		total_positions = Position.where(:positions => {player_id: self.id}).joins(:team => :game).merge(Game.completed).count
+		total_positions = self.positions.joins(:team => :game).merge(Game.completed).count
 
 		if total_positions > 0
 			(total_goals*1.0)/total_positions
@@ -50,11 +50,11 @@ class Player < ActiveRecord::Base
 	end
 
 	def player_wins
-		@player_wins ||= my_teams.merge(Team.winner).count
+		@player_wins ||= completed_teams.merge(Team.winner).count
 	end
 
 	def player_losses
-		@player_losses ||= my_teams.merge(Team.loser).count
+		@player_losses ||= completed_teams.merge(Team.loser).count
 	end
 
 	def win_percentage
@@ -69,9 +69,9 @@ class Player < ActiveRecord::Base
 	end
 
 	#teams on completed games
-	def my_teams
-		return @my_teams if defined? @my_teams
-		@my_teams ||= Team.uniq.joins(:positions).where(:positions => {:player_id => self.id}).joins(:game).merge(Game.completed)
+	def completed_teams
+		return @completed_teams if defined? @completed_teams
+		@completed_teams ||= Team.uniq.joins(:positions).where(:positions => {:player_id => self.id}).joins(:game).merge(Game.completed)
 	end
 
 	def my_goals 
@@ -82,15 +82,15 @@ class Player < ActiveRecord::Base
 	def plus_minus
 		return @plus_minus if defined? @plus_minus
 
-		opposing_teams = Team.joins(:game).where(:games => {id: my_completed_games}).where.not(:teams => {id: my_teams}).uniq
+		opposing_teams = Team.joins(:game).where(:games => {id: my_completed_games}).where.not(:teams => {id: completed_teams}).uniq
 
-		my_teams_scored_goals = Goal.scored_goal.joins(:position => :team).where(:teams => {id: my_teams}).count
-		my_teams_own_goals = Goal.own_goal.joins(:position => :team).where(:teams => {id: my_teams}).count
+		completed_teams_scored_goals = Goal.scored_goal.joins(:position => :team).where(:teams => {id: completed_teams}).count
+		completed_teams_own_goals = Goal.own_goal.joins(:position => :team).where(:teams => {id: completed_teams}).count
 
 		opposing_teams_scored_goals = Goal.scored_goal.joins(:position => :team).where(:teams => {id: opposing_teams}).count
 		opposing_teams_own_goals = Goal.own_goal.joins(:position => :team).where(:teams => {id: opposing_teams}).count
 
-		@plus_minus ||= my_teams_scored_goals + opposing_teams_own_goals - opposing_teams_scored_goals - my_teams_own_goals
+		@plus_minus ||= completed_teams_scored_goals + opposing_teams_own_goals - opposing_teams_scored_goals - completed_teams_own_goals
 	end
 
 	def margin
